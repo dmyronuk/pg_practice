44041ec563a4d917c3bc8afb0966f3a1dc3fcb63
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-06-26 20:17:01 +0000

    Clean up commented out code and remove redundant code from connection script

--- db/db.create-knex-client.js
+++ db/db.create-knex-client.js
@@ -1,14 +1,7 @@
 const settings = require("../settings"); // settings.json
 const knex = require('knex')({
   client: 'pg',
-  connection: {
-    user     : settings.user,
-    password : settings.password,
-    database : settings.database,
-    host     : settings.hostname,
-    port     : settings.port,
-    ssl      : settings.ssl
-  }
+  connection: settings
 });
 
 module.exports = knex;
--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,5 @@
 const settings = require("./settings")
 
-  //   user     : settings.user,
-  //   password : settings.password,
-  //   database : settings.database,
-  //   host     : settings.hostname,
-  //   port     : settings.port,
-  //   ssl      : settings.ssl
-
-
 module.exports = {
 
   development: {

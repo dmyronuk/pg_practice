8e6543bb9b6ec409e7af98eb9851876496724b75
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-06-26 16:14:24 +0000

    Add function to pull famous people from db Add function to format date form db result Separate logic to connect to databse into db.create-client.js

---
+++ lookup_people.js
@@ -0,0 +1,46 @@
+const client = require("./db.create-client.js")
+let cliArg = process.argv[2];
+
+// node lookup_people.js Paul
+// Searching ...
+// Found 2 person(s) by the name 'Paul':
+// - 1: Paul Rudd, born '1969-04-06'
+// - 2: Paul Giamatti, born '1967-06-06'
+
+//format y-m-d
+let formatDate = (dateObj) => {
+  let yr = dateObj.getFullYear();
+  let mo = dateObj.getMonth() + 1;
+  let day = dateObj.getDate();
+  if(mo < 10) mo = '0' + mo;
+  if(day < 10) day = '0' + day;
+  return `${yr}-${mo}-${day}`;
+}
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  console.log("Searching ...")
+
+  let queryStr = `
+    SELECT *
+    FROM famous_people
+    WHERE first_name LIKE '%${cliArg}%'
+    OR last_name LIKE '%${cliArg}%'
+  `;
+
+  client.query(queryStr, (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+
+    console.log(`Found ${result.rows.length} person(s) by the name '${cliArg}':`);
+    result.rows.forEach((elem, i) => {
+      // let d = new Date(elem.birthdate);
+      let dateStr = formatDate(elem.birthdate);
+      console.log(`- ${i + 1}: ${elem.first_name} ${elem.last_name}, born '${dateStr}'`);
+    })
+    client.end();
+  });
+});

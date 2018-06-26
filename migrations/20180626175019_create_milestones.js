071fbc6d7ac98d9c4a364ea20aa1cbf5bd01a175
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-06-26 18:50:21 +0000

    Add db migrations

--- migrations/20180626175019_create_milestones.js
+++ migrations/20180626175019_create_milestones.js
@@ -1,19 +1,15 @@
-// The milestones table should have the following fields:
-//     description (string)
-//     date_achieved (date)
-
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTable('milestones', function(table){
-      table.increments('id').primary();
-      table.string('description');
-      table.time("date_achieved");
+    knex.schema.createTable("milestones", function(table){
+      table.increments("id").primary();
+      table.string("description");
+      table.date("date_achieved");
     })
   ])
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('milestones')
+    knex.schema.dropTable("milestones")
   ])
 };
